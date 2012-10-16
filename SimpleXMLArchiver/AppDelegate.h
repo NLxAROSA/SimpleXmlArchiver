f0a2fdf629a64f69a51533497ce2afc84bac9065
Lars Rosenquist <lsrosenquist@MacBook-Air-van-Lars.local>
2012-10-16 18:54:01 +0200

    Initial Commit

---
+++ SimpleXMLArchiver/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SimpleXMLArchiver
+//
+//  Created by Lars Rosenquist on 16-10-12.
+//  Copyright (c) 2012 Lars Rosenquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SimpleXMLArchiver/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleXMLArchiver
+//
+//  Created by Lars Rosenquist on 16-10-12.
+//  Copyright (c) 2012 Lars Rosenquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
